e24b4622e7762e0c642ea7a3d04289fd6868b950
Tarcisio Nunes <github@tarcnux.com.br>
2022-04-21 18:15:04 -0300

    Adiciona espaços no Painel

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -53,11 +53,11 @@ function Panel({updateAt, onChange, data, country, getCovidData}) {
             <CardPanelContentStyled>
                 <div>
                     <Typography variant="h5" component="span" color="primary">
-                        COVID19
+                        COVID19 &nbsp;
                     </Typography>
 
                     <Typography variant="h6" component="span" color="primary">
-                        Painel Coronavírus
+                        Painel Coronavírus - &nbsp;
                     </Typography>
 
                     <Typography variant="body2" component="span" color="primary">
